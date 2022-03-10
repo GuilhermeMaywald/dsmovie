6dc82ba9e7c792bfaa8050388b7fa967e0de0b54
GuilhermeMaywald <guilhermemaywald@gmail.com>
2022-03-10 10:39:47 -0300

    Security Config

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -1,4 +1,4 @@
-import { ReactComponent as StarFull } from 'assts/img/star-full.svg';
+import { ReactComponent as StarFull } from 'assets/img/star-full.svg';
 import { ReactComponent as StarHalf } from 'assets/img/star-half.svg';
 import { ReactComponent as StarEmpty } from 'assets/img/star-empty.svg';
 import './styles.css';
